40d76334e25ce603dc31d19fcbd7f56a2d86d07c
Paul Rashidi <paulrashidi@google.com>
2016-08-15 11:30:42 -0700

    Test for AboutActivity.

---
+++ app/src/androidTest/java/com/g11x/checklistapp/AboutActivityTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright © 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.g11x.checklistapp;
+
+import android.support.test.filters.MediumTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.widget.TextView;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.core.AllOf.allOf;
+
+@MediumTest
+@RunWith(AndroidJUnit4.class)
+public class AboutActivityTest {
+  @Rule
+  public ActivityTestRule<AboutActivity> mActivity = new ActivityTestRule<>(AboutActivity.class);
+
+  @Test
+  public void activityRenders_containsNoteAboutApache2License() {
+    // Find a TextView with Apache 2.0 in the text.
+    onView(allOf(isAssignableFrom(TextView.class), withText(containsString("Apache 2.0"))))
+        // Make sure it's displayed.
+        .check(matches(isDisplayed()));
+  }
+}
