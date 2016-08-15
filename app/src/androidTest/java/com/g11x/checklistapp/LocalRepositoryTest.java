db021e412ded0549a4e8120f241e316e71295385
Waylon Calabrese <wcalabrese@wcalabrese-macbookpro2.roam.corp.google.com>
2016-08-15 14:45:32 -0500

    Add usage of Constants.java.

--- app/src/androidTest/java/com/g11x/checklistapp/LocalRepositoryTest.java
+++ app/src/androidTest/java/com/g11x/checklistapp/LocalRepositoryTest.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class LocalRepositoryTest extends ProviderTestCase2<LocalRepository> {
     public LocalRepositoryTest() {
-        super(LocalRepository.class, "com.g11x.checklistapp.provider");
+        super(LocalRepository.class, Constants.LOCAL_REPOSITORY_AUTHORITY);
     }
 
     @Override
