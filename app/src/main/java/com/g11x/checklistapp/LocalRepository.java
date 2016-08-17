79dcffdbdd06a9a451c3acd1ae9a4f82a635630f
Waylon Calabrese <wcalabrese@wcalabrese-macbookpro2.roam.corp.google.com>
2016-08-17 14:59:07 -0500

    Routing for content authority table to table manipulation logic.

--- app/src/main/java/com/g11x/checklistapp/LocalRepository.java
+++ app/src/main/java/com/g11x/checklistapp/LocalRepository.java
@@ -1,7 +1,6 @@
 package com.g11x.checklistapp;
 
 import android.content.ContentProvider;
-import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -16,38 +15,9 @@ import com.g11x.checklistapp.data.Database;
  * Application data repository.
  */
 public class LocalRepository extends ContentProvider {
-  // A string that defines the SQL statement for creating a table
-  private static final String SQL_CREATE_MAIN =
-      "CREATE TABLE important_info "
-          + "(_ID INTEGER PRIMARY KEY, INFO TEXT);";
-
-  // Helper class that actually creates and manages the provider's underlying data repository.
-  protected static final class MainDatabaseHelper extends SQLiteOpenHelper {
-    // Instantiates an open helper for the provider's SQLite data repository. Do not do database
-    // creation and upgrade here.
-    MainDatabaseHelper(Context context) {
-      super(context, Database.NAME, null, 1);
-    }
-
-    // Creates the data repository. This is called when the provider attempts to open the
-    // repository and SQLite reports that it doesn't exist.
-    public void onCreate(SQLiteDatabase db) {
-      // Creates the main table
-      db.execSQL(SQL_CREATE_MAIN);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-      // TODO: This seems useful. Learn how to use it.
-    }
-  }
-
   // Defines a handle to the database helper object. The MainDatabaseHelper class is defined in a
   // following snippet.
   private MainDatabaseHelper openHelper;
-
-  // Holds the database object
-  private SQLiteDatabase db;
 
   @Override
   public boolean onCreate() {
@@ -73,10 +43,7 @@ public class LocalRepository extends ContentProvider {
   @Nullable
   @Override
   public Uri insert(Uri uri, ContentValues contentValues) {
-    // Gets a writeable database. This will trigger its creation if it doesn't already exist.
-    db = openHelper.getWritableDatabase();
-
-    Database.TableHandler handler = Database.getTableHandler(db, uri);
+    Database.TableHandler handler = Database.getTableHandler(openHelper.getWritableDatabase(), uri);
     return handler.insert(contentValues);
   }
 
@@ -88,5 +55,26 @@ public class LocalRepository extends ContentProvider {
   @Override
   public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
     return 0;
+  }
+
+  // Helper class that actually creates and manages the provider's underlying data repository.
+  protected static final class MainDatabaseHelper extends SQLiteOpenHelper {
+    // Instantiates an open helper for the provider's SQLite data repository. Do not do database
+    // creation and upgrade here.
+    MainDatabaseHelper(Context context) {
+      super(context, Database.NAME, null, 1);
+    }
+
+    // Creates the data repository. This is called when the provider attempts to open the
+    // repository and SQLite reports that it doesn't exist.
+    public void onCreate(SQLiteDatabase db) {
+      // Creates the main table
+      db.execSQL(Database.ImportantInformation.CREATE_TABLE_SQL);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+      // TODO: This seems useful. Learn how to use it.
+    }
   }
 }
--- app/src/main/java/com/g11x/checklistapp/data/Database.java
+++ app/src/main/java/com/g11x/checklistapp/data/Database.java
@@ -19,45 +19,71 @@ package com.g11x.checklistapp.data;
 
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
-/**
- * Important domain concepts for manipulating the ImportantInformation model.
- */
+/** Important domain concepts for manipulating the ImportantInformation model. */
 public class Database {
   public static final String NAME = "g11x_checklistapp";
 
   public static TableHandler getTableHandler(SQLiteDatabase db, Uri contentUri) {
-    return new ImportantInformation.ImportantInformationTable(db, contentUri);
+    switch (URI_MATCHER.match(contentUri)) {
+      case 1:
+        return new ImportantInformation.ImportantInformationTable(db, contentUri);
+      default:
+        throw new RuntimeException(String.format("No handler registered for %s", contentUri));
+    }
   }
 
   public interface TableHandler {
     Uri insert(ContentValues contentValues);
   }
 
+  /** Information representing the ImportantInformation content..*/
   public static class ImportantInformation {
-    // Important information table content URI.
-    // TODO: Figure out how to use getString(R.string.content_provider_authority) here.
-    public static final Uri CONTENT_URI;
+    /** Important information table content URI. */
+    public static final Uri CONTENT_URI = createContentUri();
 
-    // Important information table info column.
+    /** Info column name. */
     public static final String INFO_COLUMN = "info";
+
+    /** A string that defines the SQL statement for creating a table. */
+    public static final String CREATE_TABLE_SQL = createCreateTableSql();
 
     private static final String TABLE_NAME = "important_information";
 
-    static {
-      CONTENT_URI = Uri.parse("content://com.g11x.checklistapp/" + Database.NAME + "/" + TABLE_NAME);
-    }
-
     private static class ImportantInformationTable implements TableHandler {
-      public ImportantInformationTable(SQLiteDatabase db, Uri contentUri) {}
+      private final SQLiteDatabase db;
+      private final Uri contentUri;
+
+      public ImportantInformationTable(SQLiteDatabase db, Uri contentUri) {
+        this.db = db;
+        this.contentUri = contentUri;
+      }
       
       @Override
       public Uri insert(ContentValues contentValues) {
-        long id = db.insert(Database.ImportantInformation.INFO_COLUMN, null, contentValues);
-        return ContentUris.withAppendedId(uri, id);
+        long id = db.insert(Database.ImportantInformation.TABLE_NAME, null, contentValues);
+        return ContentUris.withAppendedId(contentUri, id);
       }
     }
+
+    private static Uri createContentUri() {
+      // TODO: Figure out how to use getString(R.string.content_provider_authority) here.
+      return Uri.parse("content://com.g11x.checklistapp.provider/" + Database.NAME + "/" + TABLE_NAME);
+    }
+
+    private static String createCreateTableSql() {
+      return "create table " + TABLE_NAME + " (_ID integer primary key, " + INFO_COLUMN + " text);";
+    }
+  }
+
+  private static final UriMatcher URI_MATCHER = createContentRouter();
+
+  private static UriMatcher createContentRouter() {
+    UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+    matcher.addURI("com.g11x.checklistapp.provider", Database.NAME + "/" + ImportantInformation.TABLE_NAME, 1);
+    return matcher;
   }
 }
