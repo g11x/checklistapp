920d363ce518047d1eac43c26b57fd66ebfdc70a
Waylon Calabrese <wcalabrese@google.com>
2016-08-18 10:40:30 -0500

    moar PR comments

--- app/src/main/java/com/g11x/checklistapp/data/Database.java
+++ app/src/main/java/com/g11x/checklistapp/data/Database.java
@@ -23,26 +23,38 @@ import android.content.UriMatcher;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
-/** Important domain concepts for manipulating the ImportantInformation model. */
+/**
+ * Important domain concepts for manipulating the ImportantInformation model.
+ */
 public class Database {
   public static final String NAME = "g11x_checklistapp";
   public static final String COM_G11X_CHECKLISTAPP_PROVIDER = "com.g11x.checklistapp.provider";
 
-  /** Insert a set of content values into the database and table supplied. Handles all the
-   * mechanics of content routing for you. */
+  /**
+   * Insert a set of content values into the database and table supplied. Handles all the
+   * mechanics of content routing for you.
+   */
   public static Uri insert(SQLiteDatabase db, Uri uri, ContentValues contentValues) {
     return Database.getTableHandler(db, uri).insert(contentValues);
   }
 
-  /** Information representing the ImportantInformation content..*/
+  /**
+   * Information representing the ImportantInformation content..
+   */
   public static class ImportantInformation {
-    /** Important information table content URI. */
+    /**
+     * Important information table content URI.
+     */
     public static final Uri CONTENT_URI = createContentUri();
 
-    /** Info column name. */
+    /**
+     * Info column name.
+     */
     public static final String INFO_COLUMN = "info";
 
-    /** SQL statement for creating this table. */
+    /**
+     * SQL statement for creating this table.
+     */
     public static final String CREATE_TABLE_SQL = createCreateTableSql();
 
     private static final String TABLE_NAME = "important_information";
@@ -55,7 +67,7 @@ public class Database {
         this.db = db;
         this.contentUri = contentUri;
       }
-      
+
       @Override
       public Uri insert(ContentValues contentValues) {
         long id = db.insert(Database.ImportantInformation.TABLE_NAME, null, contentValues);
@@ -74,16 +86,24 @@ public class Database {
   }
 
   public static class ChecklistItem {
-    /** Important information table content URI. */
+    /**
+     * Important information table content URI.
+     */
     public static final Uri CONTENT_URI = createContentUri();
 
-    /** Done column name. */
+    /**
+     * Done column name.
+     */
     public static final String DONE_COLUMN = "done";
 
-    /** Item hash column name. */
+    /**
+     * Item hash column name.
+     */
     public static final String ITEM_HASH_COLUMN = "item_hash";
 
-    /** SQL statement for creating this table. */
+    /**
+     * SQL statement for creating this table.
+     */
     public static final String CREATE_TABLE_SQL = createCreateTableSql();
 
     private static final String TABLE_NAME = "checklist_item";
@@ -115,14 +135,18 @@ public class Database {
   }
 
   public static class Notification {
-    /** Important information table content URI. */
+    /**
+     * Important information table content URI.
+     */
     public static final Uri CONTENT_URI = createContentUri();
 
     public static final String TITLE_COLUMN = "title";
     public static final String MESSAGE_COLUMN = "message";
     public static final String READ_COLUMN = "item_hash";
 
-    /** SQL statement for creating this table. */
+    /**
+     * SQL statement for creating this table.
+     */
     public static final String CREATE_TABLE_SQL = createCreateTableSql();
 
     private static final String TABLE_NAME = "notification";
@@ -153,14 +177,18 @@ public class Database {
     }
   }
 
+  private static final int IMPORTANT_INFORMATION_TABLE_URI_MATCHER_INDEX = 1;
+  private static final int CHECKLIST_ITEM_TABLE_URI_MATCHER_INDEX = 2;
+  private static final int NOTIFICATION_TABLE_URI_MATCHER_INDEX = 3;
+
   private static final UriMatcher URI_MATCHER = createContentRouter();
 
   private static UriMatcher createContentRouter() {
     UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
 
-    matcher.addURI(COM_G11X_CHECKLISTAPP_PROVIDER, Database.NAME + "/" + ImportantInformation.TABLE_NAME, 1);
-    matcher.addURI(COM_G11X_CHECKLISTAPP_PROVIDER, Database.NAME + "/" + ChecklistItem.TABLE_NAME, 2);
-    matcher.addURI(COM_G11X_CHECKLISTAPP_PROVIDER, Database.NAME + "/" + Notification.TABLE_NAME, 3);
+    matcher.addURI(COM_G11X_CHECKLISTAPP_PROVIDER, Database.NAME + "/" + ImportantInformation.TABLE_NAME, IMPORTANT_INFORMATION_TABLE_URI_MATCHER_INDEX;
+    matcher.addURI(COM_G11X_CHECKLISTAPP_PROVIDER, Database.NAME + "/" + ChecklistItem.TABLE_NAME, CHECKLIST_ITEM_TABLE_URI_MATCHER_INDEX);
+    matcher.addURI(COM_G11X_CHECKLISTAPP_PROVIDER, Database.NAME + "/" + Notification.TABLE_NAME, NOTIFICATION_TABLE_URI_MATCHER_INDEX);
 
     return matcher;
   }
