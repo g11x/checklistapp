4230df22c32e63427e734219bd9c317b93eab1da
Paul Rashidi <paulrashidi@google.com>
2016-08-18 13:06:28 -0700

    Prototype language select screen.

--- app/src/main/java/com/g11x/checklistapp/LanguageActivity.java
+++ app/src/main/java/com/g11x/checklistapp/LanguageActivity.java
@@ -18,6 +18,13 @@
 package com.g11x.checklistapp;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Displays a language selection screen to allow the user to override the system defaults.
@@ -28,10 +35,53 @@ public class LanguageActivity extends NavigationActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_language);
+
+    RecyclerView recyclerView = (RecyclerView) findViewById(R.id.main_content);
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    recyclerView.setAdapter(new LanguageSelectionAdapter());
   }
 
   @Override
   protected int getNavDrawerItemIndex() {
     return NavigationActivity.NAVDRAWER_ITEM_LANGUAGE;
+  }
+
+  static class LanguageSelectionAdapter extends RecyclerView.Adapter<LanguageSelectionAdapter.ViewHolder> {
+    private String[] dataSet;
+
+    LanguageSelectionAdapter() {
+      dataSet = new String[] { "Device default (English)", "English", "Espanol", "Russian" };
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+      View v = LayoutInflater.from(parent.getContext())
+          .inflate(R.layout.activity_language_item, parent, false);
+      return new ViewHolder((CardView)v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+      holder.textView.setText(dataSet[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+      return dataSet.length;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+      private TextView textView;
+      ViewHolder(final CardView view) {
+        super(view);
+        textView = (TextView)view.findViewById(R.id.checkbox);
+        view.setOnClickListener(this);
+      }
+
+      @Override
+      public void onClick(View view) {
+
+      }
+    }
   }
 }
